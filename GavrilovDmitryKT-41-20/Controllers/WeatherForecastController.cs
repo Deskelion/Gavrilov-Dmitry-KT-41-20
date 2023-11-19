59fd307e94e5b142837c7807172f9a1c613daf6d
Гаврилов Дмитрий <amid99@yandex.ru>
2023-11-19 13:51:27 +0300

    add logger + post method

--- GavrilovDmitryKT-41-20/Controllers/WeatherForecastController.cs
+++ GavrilovDmitryKT-41-20/Controllers/WeatherForecastController.cs
@@ -21,6 +21,8 @@ namespace GavrilovDmitryKT_41_20.Controllers
         [HttpGet(Name = "GetWeatherForecast")]
         public IEnumerable<WeatherForecast> Get()
         {
+            _logger.LogError("Method was called");
+
             return Enumerable.Range(1, 5).Select(index => new WeatherForecast
             {
                 Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
@@ -29,5 +31,16 @@ namespace GavrilovDmitryKT_41_20.Controllers
             })
             .ToArray();
         }
+
+        [HttpGet(Name = "AddNewSummary")]
+        public string[] AddNewSummary(string newSummary)
+        {
+            _logger.LogError("New method was called");
+
+            var list = Summaries.ToList();
+            list.Add(newSummary);
+            return list.ToArray();
+        }
+
     }
 }
